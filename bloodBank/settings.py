01437df76f61193ee26978d7676cd0eeeded92d8
utkarshkumarvaish <143452051+utkarshkumarvaish@users.noreply.github.com>
2023-11-13 00:42:26 +0530

    Update settings.py

--- bloodBank/settings.py
+++ bloodBank/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u$_oy2(--*=@)a1#n^e=mln=c_hukq@4ek=ohp@2qd55xpsa$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
